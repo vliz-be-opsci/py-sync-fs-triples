411f516f158577e19a9c90e96e3c10b08e87462b
Marc Portier <marc.portier@vliz.be>
2024-03-30 23:27:28 +0100

    pushing this to later
    see #10 for follow up

---
+++ tests/test_main_cli.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+""" test_main_cli
+tests concerning the cli call functioning
+"""
+from util4tests import run_single_test
+
+
+def test_me():
+    pass
+
+
+if __name__ == "__main__":
+    run_single_test(__file__)
